5b137d84ee0a53eecd7ec01851ffce4c0bc96409
Bryan <bryan.schwarz564@gmail.com>
2025-03-30 20:06:24 -0500

    created a list of nodes as well as adjacency lists for the nodes and tested successfully.

--- lab_1_ChainHeal/chain_heal.c
+++ lab_1_ChainHeal/chain_heal.c
@@ -10,14 +10,26 @@ typedef struct node
 	int x, y;
 	int cur_PP, max_PP;
 	struct node *prev;
+	int adj_size;
+	struct node **adj;
+
 } Node;
 
 
-int main()
+int main(int argc, char **argv)
 {
-	int x, y, cur_PP, max_PP, node_number = 1, node_size = sizeof(Node); 
+	int x, y, z, cur_PP, max_PP, node_number = 0, node_size = sizeof(Node), x_distance, y_distance, initial_range, jump_range, num_jumps, initial_power;
+	double distance, power_reduction; 
 	Node *new_node = NULL, *temp_node = NULL;
+	Node **player_list;
 	char name[100], *name_cpy;
+
+	initial_range = atoi(argv[1]);
+	jump_range = atoi(argv[2]);
+	num_jumps = atoi(argv[3]);
+	initial_power = atoi(argv[4]);
+	power_reduction = atol(argv[5]);
+
 	while (scanf("%d", &x) == 1 && scanf("%d", &y) == 1 && scanf("%d", &cur_PP) == 1 && scanf("%d", &max_PP) == 1 && scanf("%s", &name) == 1) {
 		temp_node = new_node;
 		new_node = (Node *) malloc(node_size);	
@@ -28,10 +40,52 @@ int main()
 		name_cpy = (char *) malloc(sizeof(name) / sizeof(name[0]));
 		new_node->name = strcpy(name_cpy, name);
 		new_node->prev = temp_node;
-		if(node_number > 1) printf("%s\n", new_node->prev->name);
+		new_node->adj_size = 0;
 		node_number++;
 		printf("%d %d %d %d %s\n", new_node->x, new_node->y, new_node->cur_PP, new_node->max_PP, new_node->name);
 	}
+	printf("node number is %d\n", node_number);
+	player_list = (Node **) malloc(sizeof(Node *) * node_number);
+	x = 0;
+	while(new_node != NULL) {
+		player_list[x] = new_node;
+		new_node = new_node->prev;
+		x++;
+	}
+
+	for(x = 0; x < node_number; x++) printf("%s\n", player_list[x]->name);
+	for(x = 0; x < node_number; x++) {
+		for(y = 0; y < node_number; y++) {
+			if(x != y) {
+				x_distance = player_list[x]->x - player_list[y]->x;
+				y_distance = player_list[x]->y - player_list[y]->y;
+				distance = sqrt(x_distance*x_distance + y_distance*y_distance);
+				if(distance <= jump_range) player_list[x]->adj_size++;
+			}
+		}
+		printf("%d\n", player_list[x]->adj_size);
+		player_list[x]->adj = (Node **) malloc(sizeof(Node *) * player_list[x]->adj_size);
+		z = 0;
+		for(y = 0; y < node_number; y++) {
+			if(x != y) {
+				x_distance = player_list[x]->x - player_list[y]->x;
+				y_distance = player_list[x]->y - player_list[y]->y;
+				distance = sqrt(x_distance*x_distance + y_distance*y_distance);
+				if(distance <= jump_range) {
+					player_list[x]->adj[z] = player_list[y];
+					z++;
+				}
+				
+			}
+		}
+	}
+	for(x = 0; x < node_number; x++) {
+		printf("adj list %s\n\n", player_list[x]->name);
+		for(y = 0; y < player_list[x]->adj_size; y++) {
+			printf("%s\n", player_list[x]->adj[y]->name);
+		}
+	}
+	
 	exit(0);
 }
 
