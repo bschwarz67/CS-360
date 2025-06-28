ba897d5d63aa9f5604756a02d39d810e9cb6c798
Bryan <bryan.schwarz564@gmail.com>
2025-06-28 11:35:24 -0500

    adding in modified scripts/test binaries/ file structure to mimic functionality of tests created by Dr Plank for their servers

--- lab_4_FakeMake/src/fakemake.c
+++ lab_4_FakeMake/src/fakemake.c
@@ -11,8 +11,9 @@
 int main(int argc, char **argv)
 {
     IS is;
-    int i = 0, c_size = 0, h_size = 0, l_size = 0;
+    int i = 0, c_size = 0, h_size = 0, l_size = 0, f_size = 0;
     Dllist c, h, l, f, n;
+    char *e;
     c = new_dllist();
     h = new_dllist();
     l = new_dllist();
@@ -32,17 +33,32 @@ int main(int argc, char **argv)
                 if(is->NF > 0) { //none blank lines
                     if(strcmp(is->fields[0], "C") == 0) { //if the line starts with 'C' we put c file names into a list and keep track of the size a concatenated string would have
                         for (i = 1; i < is->NF; i++) {
-                            dll_append(c, new_jval_s(is->fields[i]));
+                            dll_append(c, new_jval_s(strdup(is->fields[i])));
                             c_size += (strlen(is->fields[i])+1);
                         }
                     }
-                    else if(strcmp(is->fields[0], "H") == 0) { //""
+                    else if(strcmp(is->fields[0], "H") == 0) { 
+                        for (i = 1; i < is->NF; i++) {
+                            dll_append(h, new_jval_s(strdup(is->fields[i])));
+                            h_size += (strlen(is->fields[i])+1);
+                        }
                     }
-                    else if(strcmp(is->fields[0], "L") == 0) { //"" 
+                    else if(strcmp(is->fields[0], "L") == 0) { 
+                        for (i = 1; i < is->NF; i++) {
+                            dll_append(l, new_jval_s(strdup(is->fields[i])));
+                            l_size += (strlen(is->fields[i])+1);
+                        }
                     }
-                    else if(strcmp(is->fields[0], "F") == 0) { //"" 
+                    else if(strcmp(is->fields[0], "F") == 0) { 
+                        for (i = 1; i < is->NF; i++) {
+                            dll_append(f, new_jval_s(strdup(is->fields[i])));
+                            f_size += (strlen(is->fields[i])+1);
+                        }
                     }
                     else if(strcmp(is->fields[0], "E") == 0) { //save the name of the executable
+                        e = (char*) malloc((strlen(is->fields[1]) + 1) * sizeof(char));
+                        strcpy(e, is->fields[1]);
+                        printf("%s\n",e);
                     }
                     else {
                         fprintf(stderr, "Error: Invalid line descriptor.\n");
