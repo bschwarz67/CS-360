1975eccfb65d4865b7678147a9ea600912954155
Bryan <bryan.schwarz564@gmail.com>
2025-06-29 22:46:19 -0500

    finished first pass at a solution. will use gradescript tomorrow to check against the correct solution.

--- lab_4_FakeMake/src/fakemake.c
+++ lab_4_FakeMake/src/fakemake.c
@@ -12,16 +12,19 @@
 int main(int argc, char **argv)
 {
     IS is;
-    int i = 0, c_size = 0, h_size = 0, l_size = 0, f_size = 0;
-    Dllist c, h, l, f, n;
-    char *e, *o, *com;
+    int i = 0, j = 0, k = 0, c_size = 0, h_size = 0, l_size = 0, f_size = 0, o_size = 0;
+    Dllist c, h, l, f, n, m, o_list;
+    char *e, *o, *com, *libs, *flags;
     struct stat o_buf;
     struct stat c_buf;
     struct stat h_buf;
+    struct stat l_buf;
+    struct stat f_buf;
     c = new_dllist();
     h = new_dllist();
     l = new_dllist();
     f = new_dllist();
+    o_list = new_dllist();
     
     
     if(argc == 1 || argc == 2) {
@@ -62,7 +65,6 @@ int main(int argc, char **argv)
                     else if(strcmp(is->fields[0], "E") == 0) { //save the name of the executable
                         e = (char*) malloc((strlen(is->fields[1]) + 1) * sizeof(char));
                         strcpy(e, is->fields[1]);
-                        printf("%s\n",e);
                     }
                     else {
                         fprintf(stderr, "Error: Invalid line descriptor.\n");
@@ -72,12 +74,9 @@ int main(int argc, char **argv)
             }
 
             
-            printf("%d\n", c_size);
             //now try to process the ages of the files to know if we need to recompile/ check the files exist
 
 
-
-            //do flags
 
 
 
@@ -101,7 +100,11 @@ int main(int argc, char **argv)
                             fprintf(stderr, "Problem compiling: %s\n", com);
                             return 0;
                         }
-                        else printf("%s\n", com);
+                        else {
+                            printf("%s\n", com);
+                            k = 1;
+
+                        }
 
                     }
                     else { //check if .c is more recent than .o, or if any of the .h files are more recent, recompile
@@ -114,17 +117,98 @@ int main(int argc, char **argv)
                                 fprintf(stderr, "Problem compiling: %s\n", com);
                                 return 0;
                             }
-                            else printf("%s\n", com);
+                            else {
+                                printf("%s\n", com);
+                                k = 1;
+                            }
                         }
-                    
+                        else {
+                            dll_traverse(m, h) {
+                                if (stat(m->val.s, &h_buf) != 0) {
+                                    fprintf(stderr, "No file found: %s\n", m->val.s);
+                                    return 0;
+                                }
+                                else {
+                                    //printf("%d %d\n",h_buf.st_mtime,o_buf.st_mtime);
+                                    if(h_buf.st_mtime > o_buf.st_mtime) {
+                                        com = malloc((10 + strlen(n->val.s)));
+                                        strcpy(com, "gcc -c -g ");
+                                        strcpy(com + 10, n->val.s);
+                                        if(system(com) != 0) {
+                                            fprintf(stderr, "Problem compiling: %s\n", com);
+                                            return 0;
+                                        }
+                                        else {
+                                            printf("%s\n", com);
+                                            k = 1;
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                }
-                
+                    dll_append(o_list, new_jval_s(strdup(o)));
+                    o_size += (strlen(o) + 1);
+                } 
             }
 
 
-            
-            
+            //do libraries
+            libs = malloc(l_size * sizeof(char));
+            i = 0;
+            dll_traverse(n, l) {
+                
+                strcpy(libs + i, n->val.s);
+                j = strlen(n->val.s);
+                strcpy(libs + i + j, " ");
+                i += (j + 1);
+            }
+            printf("libs: %s\n", libs);
+
+            //do flags
+            flags = malloc(f_size * sizeof(char));
+            i = 0;
+            dll_traverse(n, f) {
+                strcpy(flags + i, n->val.s);
+                j = strlen(n->val.s);
+                strcpy(flags + i + j, " ");
+                i += (j + 1);
+            }
+            printf("flags: %s\n", flags);
+
+            //do .o files compiled to executable
+            if(k == 1) {
+                i = 0;
+                com = malloc((10 + strlen(e) + 1 + f_size + 1 + o_size + 1 + l_size + 1) * sizeof(char));
+                strcpy(com + i, "gcc -g -o ");
+                i += 10;
+                strcpy(com + i, e);
+                i += strlen(e);
+                strcpy(com + i, " ");
+                i += 1;
+                strcpy(com + i, flags);
+                i += f_size;
+
+
+                dll_traverse(n, o_list) {
+                    strcpy(com + i, n->val.s);
+                    j = strlen(n->val.s);
+                    strcpy(com + i + j, " ");
+                    i += (j + 1);
+                }
+                strcpy(com + i, libs);
+                if(system(com) != 0) {
+                    fprintf(stderr, "Problem compiling: %s\n", com);
+                    return 0;
+                }
+                else {
+                    printf("%s\n", com);
+                }
+            }
+            else {
+                printf("Nothing to do.\n");
+            }
         }
     }
     else {
