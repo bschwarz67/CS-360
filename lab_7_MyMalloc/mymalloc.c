a426bfc26dc1c74dd9e3c45dab9422ed0f3c3306
Bryan <bryan.schwarz564@gmail.com>
2025-07-19 14:08:20 -0500

    refactoring a bit to make the code neater/ more efficient

--- lab_7_MyMalloc/mymalloc.c
+++ lab_7_MyMalloc/mymalloc.c
@@ -7,41 +7,34 @@ void *my_malloc(size_t size) {
     int rem = -1, fm_size;
 
 
-    
-
-    if(size > 8184) {
-        if(malloc_begin == NULL) {
-            rem = size % 8;
-            if(rem > 0) size += (8 - rem + 8);
-            else size += 8;
+    if(malloc_begin == NULL) {
+        rem = size % 8;
+        if(rem > 0) size += (8 - rem + 8);
+        else size += 8;
+        if(size > 8184) {
             ptr = (void *) sbrk(size);
             ptr_u_int = (unsigned int *) ptr;
             *ptr_u_int = size;
         }
+        else {
+            if(malloc_begin == NULL) {
+                ptr = (void *) sbrk(8192);
+                fm_size = 8192 - size;
 
-    }
-    else {
-        if(malloc_begin == NULL) {
-            rem = size % 8;
-            if(rem > 0) size += (8 - rem + 8);
-            else size += 8;
-            ptr = (void *) sbrk(8192);
-            fm_size = 8192 - size;
-
-
-            if(fm_size < 24) {
-                ptr_u_int = (unsigned int *) ptr;
-                *ptr_u_int = 8192;
-            }
-            else {
-                malloc_begin = (char *) ptr;
-                ptr += fm_size;
-                ptr_u_int = (unsigned int *) ptr;
-                *ptr_u_int = size;
+                if(fm_size < 24) {
+                    ptr_u_int = (unsigned int *) ptr;
+                    *ptr_u_int = 8192;
+                }
+                else {
+                    malloc_begin = (char *) ptr;
+                    ptr += fm_size;
+                    ptr_u_int = (unsigned int *) ptr;
+                    *ptr_u_int = size;
+                }
             }
         }
-        
     }
+
     return ptr + 8;
 }
 
