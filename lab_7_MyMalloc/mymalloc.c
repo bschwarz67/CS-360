ec1d46ac8f7a9dee7db93e6eca3b1d2af20cf132
Bryan <bryan.schwarz564@gmail.com>
2025-07-24 09:00:56 -0500

    very small updates to my_malloc(), tested on most of the cases that should be tricky, looks to be good but you never know until comprehensive testing.

--- lab_7_MyMalloc/mymalloc.c
+++ lab_7_MyMalloc/mymalloc.c
@@ -3,7 +3,7 @@
 void *my_malloc(size_t size) {
     void *ptr = NULL;
     unsigned int *ptr_u_int;
-    int rem = -1, fm_size;
+    int rem = -1, fm_size, size_const = size;
     Flist f, f2, prev;
 
 
@@ -58,24 +58,21 @@ void *my_malloc(size_t size) {
                         }
                         else {
                             malloc_begin = (char *) f->flink;
-                            f->flink = NULL;
                         }
                     }
                     else {
                         prev->flink = f->flink;
-                        f->flink = NULL;
                     }
                     ptr_u_int = (unsigned int *) f;
                     *ptr_u_int = f->size;
                     ptr = (void *) f;
                 }
                 else {
-                    ptr = (void *) f;
                     //take out part of the memory chunk and return, leave the rest
 
                     ptr = (void *) f; 
                     ptr += fm_size;
-                    printf("ptr: 0x%lx \n",(long unsigned int) ptr);
+                    //printf("ptr: 0x%lx \n",(long unsigned int) ptr);
 
                     ptr_u_int = (unsigned int *) ptr;
                     *ptr_u_int = size;
@@ -91,13 +88,14 @@ void *my_malloc(size_t size) {
             }
         }
         if(ptr == NULL) { //we didnt find a suitable block in the list we can just call jbrk with the size and return it.
+            size = size_const;
             if(size > 8184) {
-            rem = size % 8;
-            if(rem > 0) size += (8 - rem + 8);
-            else size += 8;
-            ptr = (void *) sbrk(size);
-            ptr_u_int = (unsigned int *) ptr;
-            *ptr_u_int = size;
+                rem = size % 8;
+                if(rem > 0) size += (8 - rem + 8);
+                else size += 8;
+                ptr = (void *) sbrk(size);
+                ptr_u_int = (unsigned int *) ptr;
+                *ptr_u_int = size;
             }
             else {
                 rem = size % 8;
@@ -107,10 +105,12 @@ void *my_malloc(size_t size) {
                 fm_size = 8192 - size;
 
                 //here we need to set the next on malloc begin to the previous malloc begin and test *****
-                malloc_begin = (char *) ptr;
+                //now we need to test this after work.
+                f2 = free_list_begin();
                 f = (Flist) ptr;
                 f->size = fm_size;
-                f->flink = NULL;
+                f->flink = f2;
+                malloc_begin = (char *) ptr;
 
                 ptr += fm_size;
                 ptr_u_int = (unsigned int *) ptr;
@@ -167,6 +167,32 @@ int main() {
     size = (int *) val; 
     printf("size of the memory we asked for at 0x%lx: %d\n", (long unsigned int) val, *size);
 
-    
+
+    val = my_malloc(5000);
+
+    f = free_list_begin();
+    while(f != NULL) {
+        printf("f: 0x%lx f size:%d\n", (long unsigned int) f, f->size);
+        f = free_list_next(f);
+    }
+    printf("pointer to the memory we asked for: 0x%lx, ", (long unsigned int) val);
+    val = val - 8;
+    size = (int *) val; 
+    printf("size of the memory we asked for at 0x%lx: %d\n", (long unsigned int) val, *size);
+
+    //test edge case when the remainder of a free memory node is > 24
+
+
+    val = my_malloc(3160);
+
+    f = free_list_begin();
+    while(f != NULL) {
+        printf("f: 0x%lx f size:%d\n", (long unsigned int) f, f->size);
+        f = free_list_next(f);
+    }
+    printf("pointer to the memory we asked for: 0x%lx, ", (long unsigned int) val);
+    val = val - 8;
+    size = (int *) val; 
+    printf("size of the memory we asked for at 0x%lx: %d\n", (long unsigned int) val, *size);
     return 0;
 }
