87313fb5dfe38d5103a92c0fc83fec4690b97c7c
Bryan <bryan.schwarz564@gmail.com>
2025-11-16 20:44:28 -0600

    completed up to multi user joining

--- lab_A_ThreadedChatServer/chat_server.c
+++ lab_A_ThreadedChatServer/chat_server.c
@@ -6,29 +6,147 @@
 #include "chat_server.h"
 #include "/home/bryan/Desktop/PersonalCodeRepo/CS-360/Libfdr/include/jrb.h"
 
-//addjrb for rooms/room struct
 
-int main(int argc, char **argv)
-{
-    int sock;
+void *client_thread(void *arg) {
+    int fd, done;
+    char clean_line[300], line[300], name[300];
+    char *room_name;
+    FILE *fin, *fout;
+    JRB tmp;
+    Dllist tmp2;
+    Client *c;
+    Room *r;
+    User *u;
+    //print possible chat rooms, get response, check if valid chatroom, if valid, put chat room struct into client struct, put fout into members, and start a thread.
+    //need to also create the list of fouts
+
+    c = (Client *) arg;
+    fin = fdopen(c->fd, "r");
+    fout = fdopen(c->fd, "w");
+    printf("%d\n",c->fd);
+    fprintf(fout, "Chat Rooms:");
+    fprintf(fout, "\n");
+    fprintf(fout, "\n");
+    fflush(fout);
     
+    jrb_traverse(tmp, c->rooms) {
+        room_name = (char *) tmp->key.v;
+        fputs(room_name, fout);
+        fprintf(fout, ":\n");
+        fflush(fout);
+    }
+
+    fprintf(fout, "Enter your chat name (no spaces):");
+    fprintf(fout, "\n");
+    fflush(fout);
+    done = 0;
+    while(!done && fgets(line, 300, fin) != NULL) {
+
+        if (sscanf(line, "%s\n", clean_line) == 1) {
+            strcpy(name, clean_line);
+            done = 1;
+            printf("name %s\n", name);
+        }
+    }
+    fprintf(fout, "Enter chat room:");
+    fprintf(fout, "\n");
+    fflush(fout);
+    done = 0;
+    while(!done && fgets(line, 300, fin) != NULL) {
+
+        if (sscanf(line, "%s\n", clean_line) == 1) {
+            printf("cleaned %s\n", clean_line);
+            tmp = jrb_find_str(c->rooms, clean_line);
+            
+
+            if (tmp == NULL) {
+                fprintf(fout, "Not a valid room.");
+                fflush(fout);
+            } 
+            else {
+                //broadast to all fd's in the room that a new user joined, add the fd to the room's fd, listen for input, put into the dlist then signal to room thread to broadcast
+                r = (Room *) tmp->val.v;
+                u = (User *) malloc(sizeof(User));
+                strcpy(u->name, name);
+                printf("i work here\n");
+                //u->fout = new_jval_v((void *) fout);
+                u->fout = fout;
+                u->fin = fin;
+                printf("her as well\n");
+                jrb_insert_int(r->members, r->n, new_jval_v((void *) u));
+                r->n++;
+                printf("here\n");
+                jrb_traverse(tmp, r->members) {
+                    u = (User *) tmp->val.v; 
+                    //fout = (FILE *) u->fout.v;
+                    fprintf(u->fout, "%s has joined\n",name);
+                    fflush(u->fout);
+                }
+                done = 1;
+                printf("correct room entered, room found, threat created\n");
+            }
+        }
+
+    
+    }
+    //need to close all hanging fin/fout in case of an interrupt
+    printf("exiting...\n");
+}
+
+
+void *room_thread(void *arg) {
+    
+
+
+}
+
+int main(int argc, char **argv) {
+    int sock, fd;
+    JRB rooms, tmp;
+    pthread_t tid, tid2;
+    Client *c;
+    Room *r;
+    char *room_name;
+
     if (argc <= 1) {
         fprintf(stderr, "usage: <port> <{chat rooms}> \n");
         exit(1);
     }
+
+    sock = serve_socket(atoi(argv[1]));
+    rooms =  make_jrb();
     for(int i = 2; i < argc; i++) {
         printf("%s\n", argv[i]);
 
         Room *r = (Room *) malloc(sizeof(Room));
+        r->members = make_jrb();
+        r->text = new_dllist();
         r->lock = (pthread_mutex_t *) malloc(sizeof(pthread_mutex_t));
         r->send = (pthread_cond_t *) malloc(sizeof(pthread_cond_t));
+        r->n = 0;
         pthread_mutex_init(r->lock, NULL);
         pthread_cond_init(r->send, NULL);
+        jrb_insert_str(rooms, argv[i], new_jval_v((void *) r));
+        pthread_create(&tid, NULL, room_thread, r);
+        r->sock = sock;
+
     }
 
-    
-    
-    sock = serve_socket(atoi(argv[1]));
+
+    while (1) {
+        fd = accept_connection(sock);
+        c = (Client *) malloc(sizeof(Client));
+        c->fd = fd;
+        c->rooms = rooms;
+        pthread_create(&tid2, NULL, client_thread, c);
+
+        
+        
+        
+
+        
+        
+    }
 
     
     
--- lab_A_ThreadedChatServer/chat_server.h
+++ lab_A_ThreadedChatServer/chat_server.h
@@ -1,8 +1,24 @@
 #include "/home/bryan/Desktop/PersonalCodeRepo/CS-360/Libfdr/include/dllist.h"
+#include "/home/bryan/Desktop/PersonalCodeRepo/CS-360/Libfdr/include/jrb.h"
 
 typedef struct {
   Dllist text;
-  Dllist members;
+  JRB members;
   pthread_mutex_t *lock;
   pthread_cond_t *send;
+  int sock;
+  int n;
 } Room;
+
+typedef struct {
+    int fd;
+    JRB rooms
+} Client;
+
+
+typedef struct {
+    char name[300];
+    //Jval fout;
+    FILE *fout;
+    FILE *fin;
+} User;
