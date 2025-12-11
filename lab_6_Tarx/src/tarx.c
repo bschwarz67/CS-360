f4347179579d442c604b3e2e478df486378e1ab7
Bryan <bryan.schwarz564@gmail.com>
2025-12-10 23:12:02 -0600

    fleshing out the while loop that reads/parses the .tarc file and recreates directories based on its contents. still have a bit of work to do on this.

--- lab_6_Tarx/src/tarx.c
+++ lab_6_Tarx/src/tarx.c
@@ -6,42 +6,86 @@
 #include "/home/bryan/Desktop/PersonalCodeRepo/CS-360/Libfdr/include/jrb.h"
 #include "/home/bryan/Desktop/PersonalCodeRepo/CS-360/Libfdr/include/dllist.h"
 
+#include <fcntl.h>
+#include <unistd.h>
 
 
+int compare(Jval v1, Jval v2)
+{
+  if (v1.l < v2.l) return -1;
+  if (v1.l > v2.l) return 1;
+  return 0;
+}
 
 
 int main(int argc, char **argv){
-    int size = 1000;
     char *buf = (char* ) malloc(sizeof(char) * 1000);
-    char *dir;
+    char *path, *bytes;
     int len;
     unsigned long inode;
     unsigned short mode, fod;
+    long mtime, size;
+    JRB inodes = make_jrb();
+    JRB tmp;
+    JRB dirs = make_jrb(); //strings
+    FILE *f;
 
-    fread(&len, 1, 4, stdin);
-    len++;
-    printf("%d\n", len);
 
-    dir = (char *) malloc(sizeof(char) * len);
-    fread(dir, 1, len - 1, stdin);
-    printf("%s\n",dir);
-
-    fread(&inode, 1, 8, stdin);
-    printf("%lu\n",inode);
-
-    fread(&mode, 1, 4, stdin);
-    printf("%o\n",mode);
-    fod = mode / 512;
-    printf("%o\n",fod);
-    printf("%d\n",fod);
-    /*
     while(!feof(stdin)) {
+        fread(&len, 1, 4, stdin);
+        len++;
+        printf("length of path: %d\n", len);
+
+        path = (char *) malloc(sizeof(char) * len);
+        fread(path, 1, len - 1, stdin);
+        printf("path: %s\n",path);
+
+        fread(&inode, 1, 8, stdin);
+        printf("inode: %lu\n",inode);
+
+        fread(&mode, 1, 4, stdin);
+        printf("mode (octal): %o\n",mode);
+        fod = mode / 512;
+        fread(&mtime, 1, 8, stdin);
+        printf("modtime: %ld\n",mtime);
+
+        if(fod == 64) { // is a file
+            tmp = jrb_find_gen(inodes, new_jval_l(inode), compare);
+            if (tmp == NULL) {
+                fread(&size, 1, 8, stdin);
+                printf("file size: %ld\n",size);
+                size++;
+                bytes = (char *) malloc(sizeof(char) * size);
+                fread(bytes, 1, size - 1, stdin);
+                printf("the bytes in the file: %s\n",bytes);
+                f = fopen(path, "w+"); //failing here when opening directories that dont have write permissions.
+                printf("test\n");
+
+                fwrite(bytes, size, 1, f);
+                free(bytes);
+                chmod(path, (mode_t) mode);
+                fclose(f);
+                
+                jrb_insert_gen(inodes, new_jval_l(inode), new_jval_s(strdup(path)), compare);
+            }
+            else {
+                link(path, tmp->val.s);
+            }
 
 
-
-        fread(buf, size, 1, stdin);
-        printf("%s\n",buf);
+        }
+        else { //is a directory
+            tmp = jrb_find_gen(inodes, new_jval_l(inode), compare);
+            if (tmp == NULL) {
+                mkdir(path, (mode_t) mode);
+                jrb_insert_gen(inodes, new_jval_l(inode), new_jval_s(strdup(path)), compare);
+            }
+            else {
+                link(path, tmp->val.s);
+            }
+        }
+        free(path);
+        
     }
-        */
 
 }
