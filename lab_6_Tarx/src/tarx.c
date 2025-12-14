906b52e9fd9726b44533d9cefa11f2ee29935032
Bryan <bryan.schwarz564@gmail.com>
2025-12-13 21:31:11 -0600

    after running gradeall i ran into some edge case failed examples, after much bit wrangling, they are all correct. lab finished i suppose :)

--- lab_6_Tarx/src/tarx.c
+++ lab_6_Tarx/src/tarx.c
@@ -40,111 +40,123 @@ int main(int argc, char **argv){
     FILE *f = NULL;
     struct utimbuf *time;
     r = fread(&len, 1, 4, stdin);
-    while(r > 0) {
-        if(flag == 1) {
-            flag = 0;
-        }
-        else {
-            r = fread(&len, 1, 4, stdin);
-            if (r < 4) break;
-
-        }
-        len++;
-
-        path = (char *) malloc(sizeof(char) * len);
-        r = fread(path, 1, len - 1, stdin);
-        if (r < len - 1) {
-            fprintf(stderr, "error!\n");
-            return 0;
-        }
-        path[len - 1] = '\0';
-
-        r = fread(&inode, 1, 8, stdin);
-        if (r < 8) {
-            fprintf(stderr, "error!\n");
-            return 0;
-        }
-
-        tmp = jrb_find_gen(inodes, new_jval_l(inode), compare);
-
-        if (tmp == NULL) {
-
-            r = fread(&mode, 1, 4, stdin);
-            if (r < 4) {
+    if(!(r < 4)) {
+        while(1) {
+            if(flag == 1) {
+                flag = 0;
+            }
+            else {
+                r = fread(&len, 1, 4, stdin);
+                if(r < 4) {
+                    if (r == 0) {
+                    break;
+                    }
+                    else {
+                        fprintf(stderr, "error!\n");
+                        return 0;
+                    }
+                }
+                
+            }
+            len++;
+            path = (char *) malloc(len);
+            r = fread(path, 1, len - 1, stdin);
+            if (r != len - 1) {
                 fprintf(stderr, "error!\n");
                 return 0;
             }
-            fod = mode / 512;
-            r = fread(&mtime, 1, 8, stdin);
+            path[len - 1] = '\0';
+
+            r = fread(&inode, 1, 8, stdin);
             if (r < 8) {
                 fprintf(stderr, "error!\n");
                 return 0;
             }
 
-            if(fod == 64) { // is a file
-                
-                
-                r = fread(&size, 1, 8, stdin);
-                if (r < 8) {
-                    fprintf(stderr, "error!\n");
-                    return 0;
-                };
-                bytes = (char *) malloc(sizeof(char) * size);
-                r = fread(bytes, sizeof(char), size, stdin);
-                if (r < size) {
+            tmp = jrb_find_gen(inodes, new_jval_l(inode), compare);
+
+            if (tmp == NULL) {
+
+                r = fread(&mode, 1, 4, stdin);
+                if (r < 4) {
                     fprintf(stderr, "error!\n");
                     return 0;
                 }
-                f = fopen(path, "w+");
-                if(f == NULL) {
+                fod = mode / 512;
+                r = fread(&mtime, 1, 8, stdin);
+                if (r < 8) {
                     fprintf(stderr, "error!\n");
                     return 0;
-                } 
+                }
+
+                if(fod == 64) { // is a file   
+                    r = fread(&size, 1, 8, stdin);
+                    if (r < 8) {
+                        fprintf(stderr, "error!\n");
+                        return 0;
+                    };
+                    bytes = (char *) malloc(sizeof(char) * size);
+                    r = fread(bytes, 1, size, stdin);
+                    if (r < size) {
+                        fprintf(stderr, "error!\n");
+                        return 0;
+                    }
+                    f = fopen(path, "w+");
+                    if(f == NULL) {
+                        fprintf(stderr, "error!\n");
+                        return 0;
+                    } 
+                    s = chmod(path, 0777);
+                    if(s == -1) {
+                        fprintf(stderr, "error!\n");
+                        return 0;
+                    }
+                    s = fwrite(bytes, sizeof(char), size, f);
+                    if(s < size) {
+                        fprintf(stderr, "error!\n");
+                        return 0;
+                    }
+                    free(bytes);
+                    fclose(f);
+
+                }
+                else { //is a directory
+                    s = mkdir(path, 0777);
+                    if(s == -1) {
+                        fprintf(stderr, "error!\n");
+                        return 0;
+                    }
+                }
+                jrb_insert_gen(inodes, new_jval_l(inode), new_jval_s(strdup(path)), compare);
+                file = (File*) malloc(sizeof(File));
+                file->path = (char *) malloc(sizeof(char) * len);
+                strcpy(file->path, path);
+                file->mode = mode;
+                file->mtime = mtime;
+                dll_append(files, new_jval_v(file));
+            }
+            else {
+                s = link(tmp->val.s, path);
+                if(s == -1) {
+                    fprintf(stderr, "error!\n");
+                    return 0;
+                }
                 s = chmod(path, 0777);
                 if(s == -1) {
                     fprintf(stderr, "error!\n");
                     return 0;
                 }
-                s = fwrite(bytes, sizeof(char), size, f);
-                if(s < size) {
-                    fprintf(stderr, "error!\n");
-                    return 0;
-                }
-                free(bytes);
-                fclose(f);
-
-            }
-            else { //is a directory
-                s = mkdir(path, 0777);
-                if(s == -1) {
-                    fprintf(stderr, "error!\n");
-                    return 0;
-                }
-                
-            }
-            jrb_insert_gen(inodes, new_jval_l(inode), new_jval_s(strdup(path)), compare);
-            file = (File*) malloc(sizeof(File));
-            file->path = (char *) malloc(sizeof(char) * len);
-            strcpy(file->path, path);
-            file->mode = mode;
-            file->mtime = mtime;
-            dll_append(files, new_jval_v(file));
+            }   
+            free(path);   
         }
-        else {
-            s = link(tmp->val.s, path);
-            if(s == -1) {
-                fprintf(stderr, "error!\n");
-                return 0;
-            }
-            s = chmod(path, 0777);
-            if(s == -1) {
-                fprintf(stderr, "error!\n");
-                return 0;
-            }
-        }   
-        free(path);
-        
     }
+    else {
+        if(ferror(stdin)) {
+            fprintf(stderr, "error!\n");
+            return 0;
+        }
+    }
+   
     dll_rtraverse(tmp2, files) {
         file = (File *) tmp2->val.v;
         time = (struct utimbuf *) malloc(sizeof(struct utimbuf));
